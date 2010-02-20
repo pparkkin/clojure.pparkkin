e03ca8f5c313c5a9374a7e16be663e26740d4631
Paavo Parkkinen <pparkkin@gmail.com>
2010-02-20 21:29:34 +0200

    Docstrings and a knapsack function

--- pparkkin/knapsack.clj
+++ pparkkin/knapsack.clj
@@ -4,6 +4,7 @@
   pparkkin.knapsack)
 
 (defn sums
+  "Return a set of all possible sums from a sequence of positive integers"
   [s]
   (loop [i s
          result #{0}]
@@ -18,6 +19,8 @@
                      result)))))
 
 (defn have-same?
+  "Do the two sets have the same element?
+  Only works for stuff that compare works on."
   [s1 s2]
   (not (nil?
         (loop [ss1 (apply sorted-set s1)
@@ -36,6 +39,7 @@
               (recur ss1 (rest ss2))))))))
 
 (defn knapsack1974
+  "The knapsack algorithm from 1974"
   [s b]
   (let [[h t] (split-at (/ (count s) 2) s)]
     (have-same?
@@ -43,3 +47,8 @@
      (set (map (fn [n]
                  (- b n))
                (sums t))))))
+
+(defn knapsack
+  "A knapsack algorithm"
+  [s b]
+  (knapsack s b))
